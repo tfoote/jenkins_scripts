f0f558f129dafcdbc5eef07c38bc8d8cff190a93
Esteve Fernandez <esteve@apache.org>
2014-01-17 19:48:02 +0100

    Backport get_jenkins_scripts_version function.

--- generate_documentation.py
+++ generate_documentation.py
@@ -8,13 +8,22 @@ import shutil
 from common import get_ros_env, call, check_output, BuildException
 from doc_stack import document_necessary, document_repo
 
+def get_jenkins_scripts_version(workspace, no_chroot=False):
+    if not no_chroot:
+        old_dir = os.getcwd()
+        os.chdir(os.path.join(workspace, 'jenkins_scripts'))
+    rev = check_output("git rev-parse HEAD").split('\n')[0]
+    if not no_chroot:
+        os.chdir(old_dir)
+    return rev
+
 def main():
     parser = argparse.ArgumentParser(description='Doc')
     parser.add_argument('rosdistro', help='The ROS distro')
     parser.add_argument('repo', help='The repository or stack name')
-    parser.add_argument('--platform', required=platform is None, help="The OS platform, e.g. 'precise'")
-    parser.add_argument('--arch', required=arch is None, help="The architecture, e.g. 'amd64'")
-    parser.add_argument('--workspace', action='store_true', help='Path where the documentation will be generated')
+    parser.add_argument('--platform', required=True, help="The OS platform, e.g. 'precise'")
+    parser.add_argument('--arch', required=True, help="The architecture, e.g. 'amd64'")
+    parser.add_argument('--workspace', required=True, help='Path where the documentation will be generated')
     parser.add_argument('--skip-garbage', action='store_true', help='Skip deleting all generated doc files')
     parser.add_argument('--force', action='store_true', help='Force generating documentation even when the upstream has not changed')
     args = parser.parse_args()
@@ -35,9 +44,18 @@ def main():
     os.makedirs(docspace)
 
     try:
+        jenkins_scripts_version = get_jenkins_scripts_version(args.workspace)
+
+        rosdoc_lite_version = checkout_rosdoc_lite(args.workspace, args.rosdistro)
+
+        necessary = document_necessary(args.workspace, docspace, args.rosdistro, args.repo,
+            rosdoc_lite_version, jenkins_scripts_version, args.force)
+        if not necessary:
+            return
+
         homepage = 'http://docs.ros.org'
-        document_repo(args.workspace, docspace, args.ros_distro, args.stack,
-                  args.platform, args.arch, homepage, False, skip_garbage)
+        document_repo(args.workspace, docspace, args.rosdistro, args.repo,
+                  args.platform, args.arch, homepage, False, True, None, None, None)
     finally:
         if not args.skip_garbage:
             shutil.rmtree(docspace)
