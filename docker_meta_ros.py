fee60bcd50d43c31b3e12574b32cfe233b8858d3
Esteve Fernandez <esteve@apache.org>
2014-01-22 23:23:49 +0100

    Cleanup

--- docker_meta_ros.py
+++ docker_meta_ros.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import os
 from subprocess import call
+import optparse
+
 
 def main():
     parser = optparse.OptionParser()
@@ -18,29 +20,21 @@ def main():
     print("Running meta_ros script for Angstrom version %s, repository %s and branch %s" % (angstrom_version,
         meta_ros_repo, meta_ros_branch))
 
-    base_path = os.path.join(options.workspace, 'setup-scripts', 'sources')
+    base_path = os.path.join(options.workspace, 'setup-scripts')
+    sources_path = os.path.join(base_path, 'sources')
     oebb_path = os.path.join(options.workspace, 'setup-scripts', 'oebb.sh')
-    with open(os.path.join(base_path, 'layers.txt'), 'r') as f1:
-        with open(os.path.join(base_path, 'layers.txt.new'), 'w') as f2:
+    with open(os.path.join(sources_path, 'layers.txt'), 'r') as f1:
+        with open(os.path.join(sources_path, 'layers.txt.new'), 'w') as f2:
             for line in f1:
                 if line.startswith('meta-ros'):
                     f2.write('meta-ros,%s,%s,HEAD' % (meta_ros_repo, meta_ros_branch))
                 else:
                     f2.write(line)
                 f2.write('\n')
-    os.rename(os.path.join(base_path, 'layers.txt.new'), os.path.join(base_path, 'layers.txt'))
-    call([oebb_path, 'bitbake', 'core-image-ros-world'])
+    os.rename(os.path.join(sources_path, 'layers.txt.new'), os.path.join(sources_path, 'layers.txt'))
+    call('cd %s && su rosbuilder -c "%s bitbake core-image-ros-world"' % (base_path, oebb_path), shell=True)
 
 if __name__ == '__main__':
     # global try
-    try:
-        main()
-        print("devel script finished cleanly")
-
-    # global catch
-    except BuildException as ex:
-        print(ex.msg)
-
-    except Exception as ex:
-        print("devel script failed. Check out the console output above for details.")
-        raise
+    main()
+    print("devel script finished cleanly")
