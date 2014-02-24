fadcac7a8c38fffae43a236a82b878acf9386604
Esteve Fernandez <esteve@apache.org>
2014-02-24 09:27:48 +0100

    Expand all paths

--- generate_meta_ros_job.py
+++ generate_meta_ros_job.py
@@ -25,70 +25,72 @@ def main():
     parser.add_argument("--rebuild", help="Discard the Docker cache and rebuild the image", action='store_true')
     args = parser.parse_args()
 
-    log_dir = os.path.join(args.workspace, 'log')
+    workspace_path = os.path.abspath(args.workspace)
+
+    log_path = os.path.join(workspace_path, 'log')
     try:
-        os.makedirs(log_dir)
+        os.makedirs(log_path)
     except OSError as ose:
         if ose.errno != errno.EEXIST:
             raise
 
-    output_dir = os.path.join(args.workspace, 'output')
+    output_path = os.path.join(workspace_path, 'output')
     try:
-        os.makedirs(output_dir)
+        os.makedirs(output_path)
     except OSError as ose:
         if ose.errno != errno.EEXIST:
             raise
 
-    build_dir = os.path.join(args.workspace, 'build')
+    build_path = os.path.join(workspace_path, 'build')
     try:
-        os.makedirs(build_dir)
+        os.makedirs(build_path)
     except OSError as ose:
         if ose.errno != errno.EEXIST:
             raise
 
-    tmp_dir = tempfile.mkdtemp()
-    base_dir = os.path.join(tmp_dir, 'jenkins_scripts')
+    tmp_path = tempfile.mkdtemp()
+    base_path = os.path.join(tmp_path, 'jenkins_scripts')
     timestamp = datetime.datetime.utcnow().strftime('%Y%m%d')
-    print('TEMPORARY DIR %s' % tmp_dir)
-    print('BASE DIR %s' % base_dir)
+    print('TEMPORARY DIR %s' % tmp_path)
+    print('BASE DIR %s' % base_path)
 
     d = {
         'operating_system': args.operating_system,
         'platform': args.platform,
         'maintainer_name': MAINTAINER_NAME,
         'maintainer_email': MAINTAINER_EMAIL,
-        'tmp_dir': tmp_dir,
-        'base_dir': base_dir,
+        'tmp_path': tmp_path,
+        'base_path': base_path,
         'timestamp': timestamp,
         'machine_type': args.machine_type,
-        'angstrom_path': args.angstrom_path,
-        'meta_ros_path': args.meta_ros_path,
-        'workspace': args.workspace,
-        'log_dir': log_dir,
-        'output_dir': output_dir,
-        'build_dir': build_dir,
+        'angstrom_path': os.path.abspath(args.angstrom_path),
+        'meta_ros_path': os.path.abspath(args.meta_ros_path),
+        'workspace': workspace_path,
+        'log_path': log_path,
+        'output_path': output_path,
+        'build_path': build_path,
         'docker_path': DOCKER_PATH,
     }
 
     cur_path = os.path.dirname(os.path.abspath(__file__))
-    shutil.copytree(cur_path, base_dir)
+    shutil.copytree(cur_path, base_path)
 
     with open(TEMPLATE_FILE) as f:
         tpl = f.read()
         res = em.expand(tpl, d)
-    with open(os.path.join(base_dir, 'Dockerfile'), 'w') as f2:
+    with open(os.path.join(base_path, 'Dockerfile'), 'w') as f2:
         f2.write(res)
-    call(['cat', '%(base_dir)s/Dockerfile' % d])
+    call(['cat', '%(base_path)s/Dockerfile' % d])
     if args.rebuild:
-        cmd = 'sudo %(docker_path)s build -no-cache -t osrf-jenkins-%(platform)s-meta-ros %(base_dir)s' % d
+        cmd = 'sudo %(docker_path)s build -no-cache -t osrf-jenkins-%(platform)s-meta-ros %(base_path)s' % d
     else:
-        cmd = 'sudo %(docker_path)s build -t osrf-jenkins-%(platform)s-meta-ros %(base_dir)s' % d
+        cmd = 'sudo %(docker_path)s build -t osrf-jenkins-%(platform)s-meta-ros %(base_path)s' % d
     print(cmd)
     call(cmd.split())
-    cmd = 'sudo %(docker_path)s run -v %(angstrom_path)s:/tmp/angstrom_src:ro -v %(meta_ros_path)s:/tmp/meta_ros_src:ro -v %(log_dir)s:/var/log:rw  -v %(output_dir)s:/home/rosbuild/workspace/output:rw osrf-jenkins-%(platform)s-meta-ros' % d
+    cmd = 'sudo %(docker_path)s run -v %(angstrom_path)s:/tmp/angstrom_src:ro -v %(meta_ros_path)s:/tmp/meta_ros_src:ro -v %(log_path)s:/var/log:rw  -v %(output_path)s:/home/rosbuild/workspace/output:rw osrf-jenkins-%(platform)s-meta-ros' % d
     print(cmd)
     call(cmd.split())
-    shutil.rmtree(tmp_dir)
+    shutil.rmtree(tmp_path)
 
 
 if __name__ == '__main__':
