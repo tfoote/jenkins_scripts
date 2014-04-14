8d1f8df093499233839f888c7f4ec620000e27bb
Tully Foote <tfoote@osrfoundation.org>
2014-04-13 22:11:06 -0700

    switch to argparse

--- generate_ubuntu_source_build.py
+++ generate_ubuntu_source_build.py
@@ -11,7 +11,7 @@ import em
 import errno
 
 from common import get_dependencies, get_package_dependencies, MAINTAINER_NAME, MAINTAINER_EMAIL, BuildException, which
-import optparse
+import argparse
 
 TEMPLATES = {
     'ubuntudeb': [ 'template_bootstrap.em', 'from_source/templates/ubuntu_deb.em'],
@@ -19,30 +19,31 @@ TEMPLATES = {
     'fedora': [ 'from_source/templates/fedora_bootstrap.em', 'from_source/templates/ubuntu_deb.em'],
     }
 
+
+available_arches = ['amd64', 'i386']
+
+
+
 def main():
-    parser = optparse.OptionParser()
-    parser.add_option('-o', '--os', default='ubuntu', dest='os')
-    parser.add_option('-p', '--platform', default='precise', dest='platform')
-    parser.add_option('-a', '--arch', default='amd64', dest='arch')
-    parser.add_option("--rebuild", action="store_true", default=False)
-    parser.add_option("--buildonly", action="store_true", default=False)
-    (options, args) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--os', default='ubuntu', dest='os')
+    parser.add_argument('-p', '--platform', default='precise', dest='platform')
+    parser.add_argument('-a', '--arch', default='amd64', dest='arch', choices=available_arches)
+    parser.add_argument("--rebuild", action="store_true", default=False)
+    parser.add_argument("--buildonly", action="store_true", default=False)
+    parser.add_argument('ros_distro')
+    parser.add_argument('workspace')
+    parser.add_argument('metapackage')
+    parser.add_argument('--template', default='ubuntudeb', choices=TEMPLATES)
 
-    #operating_system = args[0]
-    #platform = args[1]
-    #arch = args[2]
-    ros_distro = args[0]
-    workspace = args[1]
-    metapackage = args[2]
+    args = parser.parse_args()
 
-    if len(args) == 4:
-        template_tag = args[3]
-    else:
-        template_tag = 'ubuntudeb'
+    workspace = args.workspace
+    metapackage = args.metapackage
+    ros_distro = args.ros_distro
+    template_tag = args.template
 
-    if template_tag not in TEMPLATES:
-        parser.error("invalid template_tag %s" % template_tag)
-
+    # Create the workspace if it doesn't already exist
     try:
         os.makedirs(workspace)
     except OSError as e:
@@ -65,14 +66,14 @@ def main():
 
 
     d = {
-        'arch': options.arch,
+        'arch': args.arch,
         'base_dir': base_dir,
-        'buildonly': options.buildonly,
+        'buildonly': args.buildonly,
         'maintainer_email': MAINTAINER_EMAIL,
         'maintainer_name': MAINTAINER_NAME,
         'metapackage': metapackage,
-        'operating_system': options.os,
-        'platform': options.platform,
+        'operating_system': args.os,
+        'platform': args.platform,
         'ros_distro': ros_distro,
         'template_tag': template_tag,
         'timestamp': timestamp,
@@ -98,7 +99,7 @@ def main():
     call(['cat', '%(base_dir)s/Dockerfile' % d])
 
     tag = 'osrf-%(operating_system)s-%(platform)s-%(ros_distro)s-%(template_tag)s-%(metapackage)s' % d
-    if options.rebuild:
+    if args.rebuild:
         cmd = 'sudo docker build -no-cache -t %s %s' % (tag, base_dir)
     else:
         cmd = 'sudo docker build -t %s %s' % (tag, base_dir)
